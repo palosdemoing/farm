3b3a15adf3816a9b59cebe5a20d6963e141e9bfa
palosdemoing <pal.ocariz@gmail.com>
2017-09-27 23:27:18 +0200

    Intentando gestionar la relación entre Production y Animal en DTO

--- paloma/src/main/java/com/pal/farm/dao/AnimalDAO.java
+++ paloma/src/main/java/com/pal/farm/dao/AnimalDAO.java
@@ -3,12 +3,14 @@ package com.pal.farm.dao;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pal.farm.model.Animal;
 
 
+@Qualifier("AnimalDAO")
 @Transactional
 @Repository
 public interface AnimalDAO extends PagingAndSortingRepository<Animal, Integer> {
--- paloma/src/main/java/com/pal/farm/mappers/ProductionMapperServiceImpl.java
+++ paloma/src/main/java/com/pal/farm/mappers/ProductionMapperServiceImpl.java
@@ -1,20 +1,15 @@
 package com.pal.farm.mappers;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.dozer.DozerBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.pal.farm.dao.AnimalDAO;
 import com.pal.farm.dao.ProductionDAO;
-import com.pal.farm.dao.ProductionDAO;
-import com.pal.farm.dto.ProductionDTO;
 import com.pal.farm.dto.ProductionDTO;
 import com.pal.farm.model.Animal;
-import com.pal.farm.model.Production;
 import com.pal.farm.model.Production;
 
 
@@ -28,13 +23,14 @@ public class ProductionMapperServiceImpl implements ProductionMapperService {
 	public ProductionDAO productionDao;
 
 	@Autowired 
+	@Qualifier("AnimalDAO")
 	public AnimalDAO animalDao;
 	
 	@Override
 	public ProductionDTO toDTO(Production p) {
 		final ProductionDTO dto;
 		dto = mapper.map(p, ProductionDTO.class);
-		dto.setAnimal(p.getAnimal().getIdAnimal());
+//		if (p.getAnimal() != null)		dto.setAnimal(p.getAnimal().getIdAnimal());
 		return dto; 
 	}
 
@@ -43,7 +39,7 @@ public class ProductionMapperServiceImpl implements ProductionMapperService {
 		final Production p;
 		final Animal a = animalDao.findOne(dto.getAnimal());
 		p = mapper.map(dto, Production.class);
-		p.setAnimal(a);
+//		if (a != null)		p.setAnimal(a);
 		return p;
 	}
 

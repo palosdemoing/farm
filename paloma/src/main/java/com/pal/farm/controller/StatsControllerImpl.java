5537aaa2112b2840ea77a462c65add12ac3bf569
palosdemoing <pal.ocariz@gmail.com>
2017-09-29 19:24:46 +0200

    Estructura de la funcionalidad stats

--- paloma/src/main/java/com/pal/farm/controller/StatsControllerImpl.java
+++ paloma/src/main/java/com/pal/farm/controller/StatsControllerImpl.java
@@ -1,15 +1,18 @@
 package com.pal.farm.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pal.farm.dao.AnimalDAO;
 import com.pal.farm.dto.AnimalProfitsDTO;
 import com.pal.farm.dto.UserProfitsDTO;
 import com.pal.farm.service.StatsService;
@@ -28,7 +31,7 @@ public class StatsControllerImpl {
 	}
 	
 	@RequestMapping(value = "/profits/{name}", method = RequestMethod.GET)
-	public List<UserProfitsDTO> profitsByUser(@PathVariable("name") String name) throws NotFound {
-		return statsService.profitsByUser(name);
+	public List<UserProfitsDTO> profitsByUser(@PathVariable("name") String name, @RequestParam("from") Date startDate, @RequestParam("to") Date endsDate) throws NotFound {
+		return statsService.profitsByUser(name, startDate, endsDate);
 	}
 }
--- paloma/src/main/java/com/pal/farm/service/StatsServiceImpl.java
+++ paloma/src/main/java/com/pal/farm/service/StatsServiceImpl.java
@@ -1,13 +1,11 @@
 package com.pal.farm.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.pal.farm.dao.AnimalDAO;
-import com.pal.farm.dao.ProductionDAO;
-import com.pal.farm.dao.UserDAO;
 import com.pal.farm.dto.AnimalProfitsDTO;
 import com.pal.farm.dto.UserProfitsDTO;
 
@@ -16,19 +14,31 @@ import com.pal.farm.dto.UserProfitsDTO;
 public class StatsServiceImpl implements StatsService {
 
 	@Autowired
-	private UserDAO userDao;
-	
-	@Autowired
-	private AnimalDAO animalDao;
+	private UserService userService;
+
+	@Autowired 
+	public AnimalService animalService;
 
 	@Autowired
-	private ProductionDAO productionDao;
+	private ProductionService productionService;
 	
+
+	@Override
 	public List<AnimalProfitsDTO> profitsByAnimal(Integer n){
+		// get all animal con productions <> de null
+		// get productions de animal y restar el cost al offer almacenando en DTO para toda producción
+		// ordenar lista por campo profit
+		// devolver N primeros
 		return null;
 	}
 
-	public List<UserProfitsDTO> profitsByUser(String name){
+	@Override
+	public List<UserProfitsDTO> profitsByUser(String name, Date startDate, Date endsDate){
+		// get all animals de user con productions <> de null
+		// acumula todas las rpoductions entre las fichas dadas
+		// get productions de animal y restar el cost al offer almacenando en DTO para toda producción
+		// ordenar lista por campo profit
+		// devolver N primeros
 		return null;
 	}
 
